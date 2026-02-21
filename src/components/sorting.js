3a1d03047fb171528ea7f9cdbf2729ddba47a443
Egor <egor.yap@yandex.ru>
2026-02-21 17:32:46 +0300

    сделал 3 пункта

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -8,9 +8,29 @@ export function initSorting(columns) {
         if (action && action.name === 'sort') {
             // @todo: #3.1 — запомнить выбранный режим сортировки
 
+            const currentOrder = columns[action.column].order || 'none';
+            const nextOrder = sortMap[currentOrder];
+            columns[action.column].order = nextOrder;
+
+            action.dataset.value = sortMap[action.dataset.value];    // Сохраним и применим как текущее следующее состояние из карты
+            field = action.dataset.field;                            // Информация о сортируемом поле есть также в кнопке
+            order = action.dataset.value;                            // Направление заберём прямо из датасета для точности
+
             // @todo: #3.2 — сбросить сортировки остальных колонок
+            columns.forEach(column => {
+                if (column.dataset.field !== action.dataset.field) {
+                    column.dataset.value = 'none';
+                }
+            });
+
         } else {
             // @todo: #3.3 — получить выбранный режим сортировки
+            columns.forEach(column => {
+            if (column.dataset.value !== 'none') {
+                field = column.dataset.field;
+                order = column.dataset.value;
+            }
+        })
         }
 
         return sortCollection(data, field, order);
