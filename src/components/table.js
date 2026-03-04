a7b5ad7ded0d873319ac9667695d5dc6c3f6bbf9
Egor <egor.yap@yandex.ru>
2026-03-04 15:02:38 +0300

    сделаны шаги до поиска

--- src/components/table.js
+++ src/components/table.js
@@ -45,6 +45,7 @@ export function initTable(settings, onAction) {
                     row.elements[key].textContent = item[key];
                 }
             });
+            console.log(row.container)
 
             return row.container;
         });
--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ import {initTable} from "./components/table.js";
 import { initPagination } from './components/pagination.js';
 import { initSorting } from './components/sorting.js';
 import { initFiltering } from './components/filtering.js';
+import { initSearching } from './components/searching.js';
 
 // Исходные данные используемые в render()
 const {data, ...indexes} = initData(sourceData);
@@ -40,6 +41,7 @@ function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
     // @todo: использование
+    result = applySearching(result, state, action);
     result = applyFiltering(result, state, action);
     result = applySorting(result, state, action);
     result = applyPagination(result, state, action);
@@ -51,10 +53,12 @@ function render(action) {
 const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
-    before: ['header', 'filter'],
+    before: ['search', 'header', 'filter'],
     after: ['pagination']
 }, render);
 // @todo: инициализация
+
+const applySearching = initSearching(search)
 
 const applyFiltering = initFiltering(sampleTable.filter.elements, {
     searchBySeller: indexes.sellers
